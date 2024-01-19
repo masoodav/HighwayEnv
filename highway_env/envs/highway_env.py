44b5b805014a100624dee859a20ef4a50c68807d
masoodav <35439610+masoodav@users.noreply.github.com>
2024-01-19 22:38:23 +0100

    Update highway_env.py

--- highway_env/envs/highway_env.py
+++ highway_env/envs/highway_env.py
@@ -107,7 +107,7 @@ class HighwayEnv(AbstractEnv):
                 reward,
                 [
                     self.config["collision_reward"],
-                    self.config["high_speed_reward"] + self.config["right_lane_reward"],
+                    self.config["high_speed_reward"] + self.config["right_lane_reward"] + self.config["lane_change_reward"],
                 ],
                 [0, 1],
             )
@@ -131,6 +131,7 @@ class HighwayEnv(AbstractEnv):
             "right_lane_reward": lane / max(len(neighbours) - 1, 1),
             "high_speed_reward": np.clip(scaled_speed, 0, 1),
             "on_road_reward": float(self.vehicle.on_road),
+            "lane_change_reward": float(self.lane_change_reward),
         }
 
     def _is_terminated(self) -> bool:
